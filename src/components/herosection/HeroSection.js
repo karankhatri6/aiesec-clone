ec2639a92718829bb3d60147c4f0d3a19d153bc1
Karan Khatri <karankhatri535@gmail.com>
2023-01-26 21:16:47 +0530

    hero section responsive done

--- src/components/herosection/HeroSection.js
+++ src/components/herosection/HeroSection.js
@@ -4,15 +4,15 @@ import './HeroSection.css'
 
 const HeroSection = () => {
   return (
-    <div>
-        <section className='hero-section'>
+    <div className='hero-section'>
+        <section className='hero-section-box'>
             <div className='hero-heading'>
                 <h1> <span>Leadership </span>is for every young person</h1>
-                <h4>Take part in one of our programs abroad, in your city, or virtually. Start your leadership journey with AIESEC.</h4>
+                <p>Take part in one of our programs abroad, in your city, or virtually. Start your leadership journey with AIESEC.</p>
                 <button className='hero-btn'><span>Let's get started!</span></button>
             </div>
             <div className='hero-image'>
-                <img src={Heroimg} alt="Hero image"/>
+                <img src={Heroimg} alt="Hero image" />
             </div>
         </section>
     </div>
